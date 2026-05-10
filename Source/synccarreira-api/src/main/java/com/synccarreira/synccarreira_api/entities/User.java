9cb6218d51052a213a746ab10e70c77eaa0cdb56
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2026-05-09 22:58:46 -0300

    Entidades User e Role, configuração do lombok

---
+++ Source/synccarreira-api/src/main/java/com/synccarreira/synccarreira_api/entities/User.java
@@ -0,0 +1,71 @@
+package com.synccarreira.synccarreira_api.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk_usuario")
+    @EqualsAndHashCode.Include
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "nome_usuario")
+    @Getter
+    @Setter
+    private String name;
+
+    @Column(name = "email_usuario", unique = true)
+    @Getter
+    @Setter
+    private String email;
+
+    @Column(name = "senha_usuario")
+    @Getter
+    @Setter
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_usuario_role",
+            joinColumns = @JoinColumn(name = "pk_usuario"),
+            inverseJoinColumns = @JoinColumn(name = "pk_role"))
+    @Getter
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+}
