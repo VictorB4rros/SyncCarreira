c0972585fa4f7c867f7b3ec60574b396831b5ee9
Victor Barros <rodrigues.barros@aluno.ifsp.edu.br>
2026-09-22 01:38:37 -0300

    Continuação dos testes de InstitutionService

--- Source/synccarreira-api/src/test/java/com/synccarreira/synccarreira_api/tests/InstitutionFactory.java
+++ Source/synccarreira-api/src/test/java/com/synccarreira/synccarreira_api/tests/InstitutionFactory.java
@@ -1,6 +1,8 @@
 package com.synccarreira.synccarreira_api.tests;
 
 import com.synccarreira.synccarreira_api.dto.InstitutionDTO;
+import com.synccarreira.synccarreira_api.dto.InstitutionInsertDTO;
+import com.synccarreira.synccarreira_api.dto.InstitutionUpdateDTO;
 import com.synccarreira.synccarreira_api.entities.Institution;
 import com.synccarreira.synccarreira_api.entities.SchoolClass;
 import com.synccarreira.synccarreira_api.entities.enums.InstitutionType;
@@ -18,5 +20,25 @@ public class InstitutionFactory {
 
     public static InstitutionDTO createInstitutionDTO() {
         return new InstitutionDTO(createInstitution());
+    }
+
+    public static InstitutionInsertDTO createInstitutionInsertDTO() {
+        return new InstitutionInsertDTO("Escola Estadual Pedro II", "E.E. Pedro II", "33158816000105", InstitutionType.PUBLICA);
+    }
+
+    public static InstitutionInsertDTO createCustomInstitutionInsertDTO(String cnpj) {
+        return new InstitutionInsertDTO("Escola Estadual Pedro II", "E.E. Pedro II", cnpj, InstitutionType.PUBLICA);
+    }
+
+    public static InstitutionInsertDTO createInvalidInstitutionInsertDTO() {
+        return new InstitutionInsertDTO("Escola Estadual Pedro II", "E.E. Pedro II", "33179718000146", InstitutionType.PUBLICA);
+    }
+
+    public static InstitutionUpdateDTO createInstitutionUpdateDTO() {
+        return new InstitutionUpdateDTO("Escola Estadual Pedro II", "E.E. Pedro II", "33158816000105", InstitutionType.PUBLICA, Boolean.TRUE);
+    }
+
+    public static InstitutionUpdateDTO createInvalidInstitutionUpdateDTO() {
+        return new InstitutionUpdateDTO("Escola Estadual Pedro II", "E.E. Pedro II", "33179718000146", InstitutionType.PUBLICA, Boolean.TRUE);
     }
 }
